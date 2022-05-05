e1bc14f99d9826c8932dd9541408f7e3436c1832
Nikhil Kumar Singh <nikhilsinghns01@gmail.com>
2022-05-05 12:57:22 +0530

    UI Changes to Blog Page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import React from 'react';
+import React  from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import Home from './pages/Home'
 import Project from './pages/Projects';
 import Apk from './pages/Apk';
-import Api from './components/Api'
 import Navbar from './components/Navbar';
+import Blog from './pages/Blog'
 
 
 function App() {
+  
   return (
     <React.Fragment>
       <Navbar />
+      
       <Switch>
         <Route path='/' exact>
           <Home />
@@ -22,7 +24,7 @@ function App() {
           <Apk />
         </Route>
         <Route path='/blog'>
-          <Api />
+          <Blog />
         </Route>
         <Route path ='*'>
           <Redirect to='/'></Redirect>
--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,4 +1,4 @@
-import React  from 'react'
+import React ,{useState,useEffect}  from 'react'
 import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
 import CardMedia from '@mui/material/CardMedia';
@@ -10,32 +10,61 @@ import Typography from '@mui/material/Typography';
 import { red } from '@mui/material/colors';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import ShareIcon from '@mui/icons-material/Share';
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
+import './style.css'
 
+const Blog = () => {
+  const [posts,setPosts] = useState([])
+  const [loading,setLoading] = useState(false)
 
-const Blog = ({data}) => {
+  const fetchData = () => {
+    setLoading(true)
+    fetch('https://blogbackend7.herokuapp.com/getPost')
+    .then((res) => res.json())
+    .then(data => {
+      setPosts(data.posts)
+      setLoading(false)
+    }).catch(error => console.log(error))
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
     return (
       <div>
+        { loading ? 
+          <Box style={{textAlign:'center'}}>
+            <CircularProgress /> 
+            <p>Loading</p>
+          </Box> : 
+          null 
+        }
+
         {
-          data.map((post,idx) => 
-          <Card sx={{ maxWidth: 345 }} key={idx}>
-          <CardHeader 
-            avatar={
-              <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">R</Avatar>
-            } 
-            title={post.title} 
-            subheader={post.date}
-          />
-          <CardMedia component="img" height="194" image={post.imageUrl} alt="Post IMG"/>
-          <CardContent>
-            <Typography variant="body2" color="text.secondary">{post.content}</Typography>
-          </CardContent>
-          <CardActions disableSpacing>
-            <Typography variant="body2" color="text.secondary">{post.date}</Typography>
-            <IconButton aria-label="add to favorites"> <FavoriteIcon /> </IconButton>
-            <IconButton aria-label="share"> <ShareIcon /> </IconButton>
-          </CardActions>
-      
-        </Card>)
+          posts.map((post,idx) => 
+          <div class='container'>
+            <Card sx={{ maxWidth:'fit-content' }} key={idx} >
+            <CardHeader 
+              avatar={
+                <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">R</Avatar>
+              } 
+              title={post.title} 
+              subheader={post.author}
+            />
+            <CardMedia component="img" height="194" image={post.imageUrl} alt="Post IMG"/>
+            <CardContent>
+              <Typography variant="body2" color="text.secondary">{post.content}</Typography>
+            </CardContent>
+            <CardActions disableSpacing>
+              <Typography variant="body2" color="text.secondary">{new Date(post.date).toDateString()}</Typography>
+              <IconButton aria-label="add to favorites"> <FavoriteIcon /> </IconButton>
+              <IconButton aria-label="share"> <ShareIcon /> </IconButton>
+            </CardActions>
+          </Card>
+          </div>
+        )
         }
       </div>
   );
